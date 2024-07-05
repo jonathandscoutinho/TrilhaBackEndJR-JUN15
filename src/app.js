facb08a57c4e867a2edabe38d153cdfe795a30cc
Jonathan Coutinho <jonathandscoutinho@gmail.com>
2024-07-04 21:19:30 -0300

    add SSL

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import express from 'express'
+import fs from 'fs'
+import https from 'https'
+import cors from 'cors'
+
 const app = express()
 app.use(express.json())
+app.use(cors())
+
 
 import router from './routes/routes.js'
 app.use(router)
 
-app.listen(3000, ()=> console.log('API Rodando.'))
+//app.listen(3000, ()=> console.log('API Rodando.'))
+
+https.createServer({
+    cert: fs.readFileSync('SSL/code.crt'),
+    key: fs.readFileSync('SSL/code.key')
+}, app).listen(3000, ()=> console.log('server run :3000'))
